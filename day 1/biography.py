21116a8628d31b03b3409cf56bae7547939dd4f2
VacheKakalashvili <vkakalashvili1@gmail.com>
2023-09-25 17:19:32 +0400

    homeworks

---
+++ day 1/biography.py
@@ -0,0 +1,24 @@
+name = "vache"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნჭებელივსიმბოლო
+# "vache" არის ცვლადის მნიშვნელობა
+
+surname = "kakalashvili"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "vache" #ეს არის str (string) ტიპის ცვლადი
+age = 13 # ეს არის int ( integer ) მთელი რიცხვი
+height = 160 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+name = "vache "
+surname = "kakalashvili "
+age = "13 "
+height = "160 "
+
+print(name)
+print(surname)
+print(height)
+print(age)
