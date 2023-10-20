0796fd2bbc2f5a2f577274a7167a6b9b50aa2f83
VacheKakalashvili <143308879+VacheKakalashvili@users.noreply.github.com>
2023-10-20 21:09:55 +0400

    Add files via upload

---
+++ homework/homework.py
@@ -0,0 +1,3 @@
+percent = float(input("what is the bank rate: "))
+cash = float(input("how much money is in your bank account: "))
+print("ორ წელიწადში შენ გექნება ", cash * percent * float(24), "ლარი")
