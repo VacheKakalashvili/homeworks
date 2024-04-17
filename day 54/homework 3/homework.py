e7d0c390771ee53bacfb29ec35ce6866a0cf6011
VacheKakalashvili <143308879+VacheKakalashvili@users.noreply.github.com>
2024-04-17 10:15:26 +0400

    Add files via upload

---
+++ day 54/homework 3/homework.py
@@ -0,0 +1,20 @@
+me = 13
+brother = 21
+mother = 48
+father = 48
+aunt = 53
+grandmother = 71
+
+my_age_after_25_years = me + 25
+brother_age_after_25_years = brother + 25
+mother_age_after_25_years = mother + 25
+father_age_after_25_years = father + 25
+aunt_age_after_25_years = aunt + 25
+grandmother_age_after_25_years = grandmother + 25
+
+print("My age after 25 years:", my_age_after_25_years)
+print("Brother's age after 25 years:", brother_age_after_25_years)
+print("Mother's age after 25 years:", mother_age_after_25_years)
+print("Father's age after 25 years:", father_age_after_25_years)
+print("Aunt's age after 25 years:", aunt_age_after_25_years)
+print("Grandmother's age after 25 years:", grandmother_age_after_25_years)
---
+++ day 54/homework 4/homework.py
@@ -0,0 +1,11 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+age = input("Enter your age: ")
+place_of_residence = input("Enter your place of residence: ")
+profession = input("Enter your profession: ")
+hobbies = input("Enter your hobbies: ")
+additional_info = input("Enter any additional information: ")
+
+sentence = f"My name is {first_name} {last_name}. I am {age} years old and I live in {place_of_residence}. I work as a {profession} and my hobbies include {hobbies}. {additional_info}"
+
+print(sentence)
