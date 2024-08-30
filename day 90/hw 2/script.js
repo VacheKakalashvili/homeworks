c16a8c4f75be52dc55da178227c3beb911da61fb
VacheKakalashvili <143308879+VacheKakalashvili@users.noreply.github.com>
2024-08-30 14:46:00 +0400

    Add files via upload

---
+++ day 90/hw 2/script.js
@@ -0,0 +1,13 @@
+let person = {
+    firstName: "Vache",
+    lastName: "Kakalashvili",
+    age: 14,
+    gender: "Male",
+    address: {
+        street: "T.Tabidze 74",
+        city: "Tbilisi",
+        country: "Georgia"
+    }
+};
+
+console.log(person);
